7b12fa63bec8a0895aee1887f0f800f3780a1142
Clark Laurent Dumon <clarklaurent.dumon@outlook.com>
2024-08-04 00:23:51 +0800

    got pokeMax issue pokelist

--- js/script.js
+++ js/script.js
@@ -1,16 +1,29 @@
 console.clear;
 console.log("hello");
-
-const apiPokemon = () => {
-  const apiPokeDEX = [];
-
-  fetch("https://pokeapi.co/api/v2/pokemon/1")
+let pokeDEX = [];
+// My function
+const myfunction = async function () {
+  let pokeMax = fetch(`https://pokeapi.co/api/v2/pokemon?limit=1500`)
     .then((respose) => {
       return respose.json();
     })
     .then((pokeData) => {
       console.log(pokeData);
+      return pokeData.count;
     });
+
+  for (let i = 1; i < (await pokeMax); i++) {
+    pokeDEX.push(
+      fetch(`https://pokeapi.co/api/v2/pokemon/${i}`)
+        .then((response) => {
+          return response.json();
+        })
+        .then((pokeData) => {
+          return pokeData;
+        })
+    );
+  }
 };
 
-apiPokemon();
+// Start function
+myfunction();
