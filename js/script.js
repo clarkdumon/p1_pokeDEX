e3ef93dcc139986981ab406b5c2be0dbbeac0ad7
Clark Laurent Dumon <clarklaurent.dumon@outlook.com>
2024-08-05 16:50:20 +0800

    update completed pokemon details

--- js/script.js
+++ js/script.js
@@ -9,59 +9,63 @@ const myfunction = async function () {
       return response.json();
     })
     .then((pokeData) => {
+      //   console.log(pokeData);
+
       const pokeMon = {
-        name: pokeData.name,
         index: pokeData.id,
-        weight: pokeData.weight,
-        height: pokeData.height,
+        name: pokeData.name,
         image: pokeData.sprites.other.home.front_default,
-        stats: {
-          "hp": pokeData.stats[0].base_stat,
-          "attack": pokeData.stats[1].base_stat,
-          "defense": pokeData.stats[2].base_stat,
-          "special-attack": pokeData.stats[3].base_stat,
-          "special-defense": pokeData.stats[4].base_stat,
-          "speed": pokeData.stats[5].base_stat,
-        },
+        height: pokeData.height,
+        weight: pokeData.weight,
+        type: pokeData.types.map((type) => {
+          return type.type.name;
+        }),
+        stats: pokeData.stats.map((stat) => {
+          return [stat.stat.name, stat.base_stat];
+        }),
+        // thestats: pokeData.stats.map((stat) => {
+        //   console.log((stat.base_stat.stat.name + stat.base_stat)
+        //   status.assign, );
+        // }),
       };
       console.log(pokeMon);
     });
-
-  //   let pokeDEX = [];
-  //   let pokeMax = fetch(`https://pokeapi.co/api/v2/pokemon?limit=1500`)
-  //     .then((respose) => {
-  //       return respose.json();
-  //     })
-  //     .then((pokeData) => {
-  //       return pokeData.results;
-  //     })
-  //     .then((results) => {
-  //       let url = results.map((result) => result.url);
-  //       return url;
-  //     })
-  //     .then((pokeMon) => {
-  //       pokeDEX.push(
-  //         pokeMon.forEach((poke) => {
-  //           fetch(poke).then((reponse) => {
-  //             return reponse.json();
-  //           });
-  //         })
-  //       );
-  //       console.log(pokeDEX);
-  //     });
-
-  //   for (let i = 1; i < (await pokeMax); i++) {
-  //     pokeDEX.push(
-  //       fetch(`https://pokeapi.co/api/v2/pokemon/${i}`)
-  //         .then((response) => {
-  //           return response.json();
-  //         })
-  //         .then((pokeData) => {
-  //           return pokeData;
-  //         })
-  //     );
-  //   }
 };
+
+//   let pokeDEX = [];
+//   let pokeMax = fetch(`https://pokeapi.co/api/v2/pokemon?limit=1500`)
+//     .then((respose) => {
+//       return respose.json();
+//     })
+//     .then((pokeData) => {
+//       return pokeData.results;
+//     })
+//     .then((results) => {
+//       let url = results.map((result) => result.url);
+//       return url;
+//     })
+//     .then((pokeMon) => {
+//       pokeDEX.push(
+//         pokeMon.forEach((poke) => {
+//           fetch(poke).then((reponse) => {
+//             return reponse.json();
+//           });
+//         })
+//       );
+//       console.log(pokeDEX);
+//     });
+
+//   for (let i = 1; i < (await pokeMax); i++) {
+//     pokeDEX.push(
+//       fetch(`https://pokeapi.co/api/v2/pokemon/${i}`)
+//         .then((response) => {
+//           return response.json();
+//         })
+//         .then((pokeData) => {
+//           return pokeData;
+//         })
+//     );
+//   }
 
 // Start function
 myfunction();
