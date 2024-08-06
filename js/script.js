15a8d235b1e62d1d3cb3a55288c7e1bec291c18f
Clark Laurent Dumon <clark.dumon@gmail.com>
2024-08-06 15:07:01 +0000

    update

--- js/script.js
+++ js/script.js
@@ -1,21 +1,29 @@
-console.clear
+console.clear;
 
-const pokeList = document.getElementById('pokemonList') 
-const pokeSearch = document.getElementById('seach-box')
-console.log(pokeSearch)
+const pokeList = document.getElementById("pokemonList");
+const pokeSearch = document.getElementById("seach-box");
+console.log(pokeSearch);
 
-pokeSearch.addEventListener('keyup',(event)=>{
-    const searchValue = event.target.value
-    fetch(`https://pokeapi.co/api/v2/pokemon?limit=1500`).then(response => response.json()).then(result=>{
-        listPokemon( result.results.map(pokemon))
-    })
+pokeSearch.addEventListener("keyup", (event) => {
+  const searchValue = event.target.value;
+  fetch(`https://pokeapi.co/api/v2/pokemon?limit=1500`)
+    .then((response) => response.json())
+    .then((result) => {
+      listPokemon(result.results.map(pokemon));
+    });
 
-    const listPokemon = (pokemonList) =>{
-    console.log(pokemonList)
-    
+  const listPokemon = (pokemonList) => {
+    console.log(pokemonList);
 
-    const listHtml = pokemonList.map(pokemon => `<li>${pokemon.name.charAt(0).toUpperCase()+pokemon.name.substring(1).toLowerCase()}</li>`).join("")
-    pokeList.innerHTML=listHtml
-}
-})
-
+    const listHtml = pokemonList
+      .map(
+        (pokemon) =>
+          `<li>${
+            pokemon.name.charAt(0).toUpperCase() +
+            pokemon.name.substring(1).toLowerCase()
+          }</li>`
+      )
+      .join("");
+    pokeList.innerHTML = listHtml;
+  };
+});
