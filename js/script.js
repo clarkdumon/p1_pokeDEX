e76e6d1dc25651d25d28834482dff538e6b82829
Clark Laurent Dumon <clark.dumon@gmail.com>
2024-08-19 15:24:30 +0000

    slowy taking place

--- js/script.js
+++ js/script.js
@@ -1,76 +1,50 @@
 console.clear;
+const pokeList = document.getElementById("pokemonList");
+const searchBar = document.getElementById("searchBox");
 
-const txtSearchBox = document.getElementById("search-box");
-const btnSearch = document.getElementById("btn-search");
-const pokemonList = document.getElementById("pokemonList");
-
-btnSearch.addEventListener("click", funcbtnSearch);
-
-function pushPokesearch(pokemon) {
-	pokemonList.innerHTML = "";
-	txtSearchBox.value = pokemon;
-}
-
-txtSearchBox.addEventListener("focus", funcpokemonList);
-txtSearchBox.addEventListener("keyup", funcpokemonList);
-function funcbtnSearch() {
-	console.log(txtSearchBox.value);
-	fetchPokemon(txtSearchBox.value);
-}
-async function funcpokemonList() {
-	pokemonList.innerHTML = await seachbarFunc(txtSearchBox.value);
-}
+searchBar.addEventListener("keyup", fetchPokemonList);
 
 async function fetchData(url, funcName) {
 	//
 	try {
 		const response = await fetch(url);
 		if (!response.ok) {
-			throw new Error("Func:" + funcName + " | Network response was not ok " + response.statusText);
+			throw new Error(
+				"Func:" +
+					funcName +
+					" | Network response was not ok " +
+					response.statusText
+			);
 		}
 		const data = await response.json();
 		return data;
 	} catch (error) {
-		console.error("Func:" + funcName + " | There has been a problem with your fetch operation:", error);
+		console.error(
+			"Func:" +
+				funcName +
+				" | There has been a problem with your fetch operation:",
+			error
+		);
 	}
 }
 
-async function fetchPokemon(pokemon) {
-	const url = `https://pokeapi.co/api/v2/pokemon/${pokemon}`; // URL String used to fetch data from API
-	const data = await fetchData(url, "fetchPokemon"); // Callback function to post response and return data
-
-	const pokemonData = {
-		index: data.id,
-		name: data.name,
-		sprite: data.sprites.other["official-artwork"].front_default,
-		type: data.types.map((type) => {
-			return type.type.name;
-		}),
-		height: data.height,
-		weight: data.weight,
-		stats: data.stats.map((stat) => {
-			const status = { [stat.stat.name]: stat.base_stat };
-			return status;
-		}),
-	};
-	console.log(pokemonData);
-	return pokemonData;
-}
-
-async function fetchPokemonList(keys) {
+async function fetchPokemonList() {
 	const url = "https://pokeapi.co/api/v2/pokemon?limit=15000";
-	let searchValue = keys; // let searchValue = keys.target.value;
+	let searchValue = searchBar.value;
 	let data = await fetchData(url, "fetchPokemonList");
 	let pokemons = data.results.map((res) => {
 		return res.name;
 	});
 
 	let pokemonList = pokemons.filter((name) => {
-		if (name.includes(keys)) {
+		if (name.includes(searchValue)) {
 			return name;
 		}
 	});
-	return pokemonList;
+	let searchHTML = pokemonList.map((poke) => {
+		return `<li class="pokemon" onclick="pushPokesearch('${poke}')">${poke}</li>`;
+	});
+	pokeList.innerHTML = searchHTML.join("");
 }
 
 async function seachbarFunc(params) {
